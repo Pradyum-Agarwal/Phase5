2ef1a968311b1236794ab8a345d4b31042b5b902
Pradyum <96417698+Pradyum-Agarwal@users.noreply.github.com>
2022-04-11 14:53:56 +0530

    Add files via upload

---
+++ phase-five-main/src/main/java/com/example/phasefive/PhaseFiveApplication.java
@@ -0,0 +1,15 @@
+package com.example.phasefive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.http.HttpRequest;
+
+@SpringBootApplication
+public class PhaseFiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhaseFiveApplication.class, args);
+    }
+
+}
